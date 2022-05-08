597bc1abc3ef35eafac35ae2270fe147bf091e4f
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-05-08 13:42:07 -0300

    Introdução a arquivos estáticos.

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'alurareceita.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'receitas' / 'templates' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / "alurareceita" / "static", 
+    # Repare que os arquivos estáticos estão na pasta do projeto, não do app.
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
